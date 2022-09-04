c7f59cb21281670592dce2b9145bc0660deca06c
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-09-04 20:36:45 +0700

    Simple Ecommerce

--- assets/source.js
+++ assets/source.js
@@ -1,5 +1,16 @@
-document.getElementById("year").innerHTML = new Date().getFullYear();
+document.getElementById("year").innerHTML = 
+new Date().getFullYear();
 
-function buy(product_name) {
-    alert(product_name);
+function buy(product_name, product_price) {
+    const message = `Halo saya mau beli ${product_name} - ${product_price} !`;
+    const whatsapp = `https://wa.me/6287882770761?text=${message}`;
+    open(whatsapp, "_blank");
+    //location.href = whatsapp;
+}
+
+function chat() {
+    const message = `Halo, Saya ingin bertanya?`;
+    const whatsapp = `https://wa.me/6287882770761?text=${message}`;
+    open(whatsapp, "_blank");
+    //alert('Halo, admin saya mau tanya?');
 }
